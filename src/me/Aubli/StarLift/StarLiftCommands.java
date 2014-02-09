2db9f6023b46b3ecf23bd36fe6207842359101f4
AlexMl <alexandermlinzk@gmail.com>
2014-02-09 16:34:17 +0100

    Add remove Command

--- src/me/Aubli/StarLift/StarLiftCommands.java
+++ src/me/Aubli/StarLift/StarLiftCommands.java
@@ -1,5 +1,7 @@
 package me.Aubli.StarLift;
 
+import java.io.File;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -44,6 +46,29 @@ public class StarLiftCommands implements CommandExecutor{
 				}
 			}
 			
+			if(args.length==2){
+				if(args[0].equalsIgnoreCase("remove")){
+					if(playerSender.hasPermission("sl.remove")){
+						File liftFile = new File(plugin.liftPath + args[1] + ".yml");
+						
+						if(liftFile.exists()){
+							liftFile.delete();
+							plugin.liftStats.remove(Integer.parseInt(args[1]));
+							plugin.saveLifts();
+						}else{
+							playerSender.sendMessage(plugin.messagePrefix + ChatColor.RED + "Der Lift " + args[1] + " existiert nicht!");
+							return true;
+						}
+					}else{
+						playerSender.sendMessage(ChatColor.DARK_RED + "You don't have permissions for that command!");
+						return true;
+					}
+				}else{
+					printCommands(playerSender);
+					return true;
+				}
+			}
+			
 			if(args.length==3){
 				if(args[0].equalsIgnoreCase("lift")){
 					plugin.lift(playerSender, Integer.parseInt(args[1]), Integer.parseInt(args[2]));
@@ -65,8 +90,10 @@ public class StarLiftCommands implements CommandExecutor{
 			String pluginVersion = plugin.getDescription().getVersion();
 			String pluginName = plugin.getDescription().getName();
 			
-			playerSender.sendMessage(ChatColor.YELLOW + "|---------- " + pluginName + " " + pluginVersion + " ----------|");
-			playerSender.sendMessage("| sl tool");
+			playerSender.sendMessage(ChatColor.YELLOW + "|---------- " + pluginName + " v" + pluginVersion + " ----------|");
+			playerSender.sendMessage(ChatColor.YELLOW + "|" + ChatColor.DARK_GRAY + " sl tool");
+			playerSender.sendMessage(ChatColor.YELLOW + "|" + ChatColor.DARK_GRAY + " sl remove [LiftID]");
+			playerSender.sendMessage(ChatColor.YELLOW + "|" + ChatColor.DARK_GRAY + " sl list");
 		}else{
 			playerSender.sendMessage(ChatColor.DARK_RED + "You don't have permissions for that command!");
 			return;
